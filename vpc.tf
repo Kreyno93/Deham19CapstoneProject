438cb16a069f4fdd7cc91363995a9daad6645a39
Kreyno93 <michael.wagener@neuefische.de>
2024-11-15 15:13:39 +0100

    adding a public subnet for the upcoming wordpress server

--- vpc.tf
+++ vpc.tf
@@ -9,3 +9,14 @@ resource "aws_vpc" "main" {
   }
 }
 
+# Public Subnets
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-central-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-wordpress-1"
+  }
+}
