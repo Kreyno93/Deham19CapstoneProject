6c6e2ab9a26572beacfc88ec7a964e032a5c37b4
Kreyno93 <michael.wagener@neuefische.de>
2024-11-15 15:20:58 +0100

    [FIX] adjusting the CIDR Block

--- vpc.tf
+++ vpc.tf
@@ -24,7 +24,7 @@ resource "aws_subnet" "public_1" {
 # Public Subnet 2
 resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0" 
+  cidr_block              = "10.0.2.0/28" 
   availability_zone       = "us-west-2"
   map_public_ip_on_launch = true
 
