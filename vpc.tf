94c5205d1a6048df7a358060be8f95c6de61e5ee
Kreyno93 <michael.wagener@neuefische.de>
2024-11-15 15:19:25 +0100

    error message force

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 # VPC Configuration
 resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16" 
   enable_dns_hostnames = true
   enable_dns_support   = true
 
@@ -12,7 +12,7 @@ resource "aws_vpc" "main" {
 # Public Subnets
 resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.1.0/24" 
   availability_zone       = "eu-central-1a"
   map_public_ip_on_launch = true
 
@@ -20,3 +20,16 @@ resource "aws_subnet" "public_1" {
     Name = "public-wordpress-1"
   }
 }
+
+# Public Subnet 2
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0" 
+  availability_zone       = "us-west-2"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-wordpress-2"
+  }
+}
+
